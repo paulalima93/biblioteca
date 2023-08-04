9424573fba3b55767c978f088e40cd85762d18d3
paulalima93 <91716314+paulalima93@users.noreply.github.com>
2023-08-04 15:29:40 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import BottomTabNavigator from "./navigation/BottomTabNavigator";
+
+export default class App extends Component {
+  render() {
+    return <BottomTabNavigator />;
+  }
+}
---
+++ screens/Transaction.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import { Text, View } from "react-native";
+
+export default class Transaction extends Component{
+    render(){
+        return(
+            <View>
+                <Text>Tela Transação</Text>
+            </View>
+        )
+    }
+}
