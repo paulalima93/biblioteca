5fcef72ca28c93e705a2c6204efb7d0632fe8d69
paulalima93 <91716314+paulalima93@users.noreply.github.com>
2023-10-04 15:57:46 -0300

    att

--- App.js
+++ App.js
@@ -1,8 +1,46 @@
 import React, { Component } from "react";
-import BottomTabNavigator from "./navigation/BottomTabNavigator";
+import { Rajdhani_600SemiBold } from "@expo-google-fonts/rajdhani";
+import * as Font from "expo-font";
+import LoginScreen from "./screens/Login";
+import BottomTabNavigator from "./components/BottomTabNavigator";
+import { createSwitchNavigator, createAppContainer } from "react-navigation";
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      fontLoaded: false
+    };
+  }
+
+  async loadFonts() {
+    await Font.loadAsync({
+      Rajdhani_600SemiBold: Rajdhani_600SemiBold
+    });
+    this.setState({ fontLoaded: true });
+  }
+
+  componentDidMount() {
+    this.loadFonts();
+  }
+
   render() {
-    return <BottomTabNavigator />;
+    const { fontLoaded } = this.state;
+    if (fontLoaded) {
+      return <AppContainer />;
+    }
+    return null;
   }
 }
+
+
+const AppSwitchNavigator = createSwitchNavigator({
+  Login: {
+    screen: LoginScreen
+  },
+  BottomTab: {
+    screen: BottomTabNavigator
+  }
+}, { initialRouteName: "Login"})
+
+const AppContainer = createAppContainer(AppSwitchNavigator);
---
+++ config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCaTgxpCVdEXUw43ZTfqoiTDC5PeLg3ya0",
+  authDomain: "biblioteca-igor.firebaseapp.com",
+  projectId: "biblioteca-igor",
+  storageBucket: "biblioteca-igor.appspot.com",
+  messagingSenderId: "130491347765",
+  appId: "1:130491347765:web:bda30c36b8f88326e1927c",
+  measurementId: "G-H6LWZJ0BFB"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
